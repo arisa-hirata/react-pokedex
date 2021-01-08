7939e092e4ea6c7d8afc1631cbf19b27de1d2aea
“arisa-hirata” <arisa.pirata@gmail.com>
2021-01-07 22:40:29 -0800

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import pokeball from './assets/pokeball.svg';
+
+const Header = () => {
+  return (
+    <div>
+      <nav
+        className="navbar navbar-expand-md fixed-top"
+        style={{
+          backgroundColor: '#e3350f',
+          height: 60,
+          justifyContent: 'center',
+          display: 'flex',
+          alignItems: 'center'
+        }}>
+        <a
+          href="/"
+          className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center"
+        >
+          <img src={pokeball} style={{width: 40}}/>
+        </a>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
