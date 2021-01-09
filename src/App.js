6edf45248855ebdd227f416bf7d6b8066ccaf4c7
“arisa-hirata” <arisa.pirata@gmail.com>
2021-01-09 15:10:42 -0800

    adeed type color

--- src/App.js
+++ src/App.js
@@ -4,12 +4,34 @@ import axios from "axios";
 import Header from './components/Header';
 import './App.css';
 
+const TYPE_COLORS = {
+  bug: 'B1C12E',
+  dark: '4F3A2D',
+  dragon: '755EDF',
+  electric: 'FCBC17',
+  fairy: 'F4B1F4',
+  fighting: '823551D',
+  fire: 'E73B0C',
+  flying: 'A3B3F7',
+  ghost: '6060B2',
+  grass: '74C236',
+  ground: 'D3B357',
+  ice: 'A3E7FD',
+  normal: 'C8C4BC',
+  poison: '934594',
+  psychic: 'ED4882',
+  rock: 'B9A156',
+  steel: 'B5B5C3',
+  water: '3295F6'
+};
+
 function App() {
   // const [pokemon, setPokemon] = useState("");
   // const [pokemonData, setPokemonData] = useState({});
   const [pokemon, setPokemon] = useState("");
   const [pokemonData, setPokemonData] = useState([]);
   const [pokemonType, setPokemonType] = useState("");
+  const [typeColor, setTypeColor] = useState("");
 
   const getPokemonCard = async () => {
     const pokeArr = [];
@@ -19,6 +41,10 @@ function App() {
       const pokemonRes = await axios.get(pokemonUrl);
 
       const imageUrl = pokemonRes.data.sprites.front_default;
+      const types = pokemonRes.data.types.map(type => type.type.name);
+      const themeColor = `${TYPE_COLORS[types[types.length - 1]]}`;
+      console.log("themeColor: ", themeColor);
+      setTypeColor(typeColor)
       // console.log(pokemonRes.data);
       pokeArr.push(pokemonRes.data);
       // setPokemonType(pokemonRes.data.types[0].type.name);
@@ -80,11 +106,25 @@ function App() {
                 minWidth: 500,
                 height: 500
               }}>
-              <CardHeader
-                subheader= {data.id}
-                title={data.name}
-                // subheader={}
-              />
+              <div className="card-header">
+                <h5># {data.id}</h5>
+                <h3>
+                  {data.name.split(' ').map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(' ')}
+                </h3>
+                <div>
+                  Type: &nbsp;
+                  {data.types.map(type => (
+                    <span
+                      key={type}
+                      style={{
+                        backgroundColor: `#${TYPE_COLORS[type.type.name]}`,
+                        color: 'white'
+                      }}
+                    >{type.type.name}&nbsp;</span>
+                  ))}
+
+                </div>
+              </div>
               <div className="pokemon-data">
 
                 <div className="basic-data">
@@ -98,27 +138,27 @@ function App() {
                     <div className="status-container">
                       <div className="status">
                         <div>HP: </div>
-                        <div>{data.stats[0].base_stat}</div>
+                        <div className="status-details">{data.stats[0].base_stat}</div>
                       </div>
                       <div className="status">
                         <div>Attack: </div>
-                        <div>{data.stats[1].base_stat}</div>
+                        <div className="status-details">{data.stats[1].base_stat}</div>
                       </div>
                       <div className="status">
                         <div>Defense: </div>
-                        <div>{data.stats[2].base_stat}</div>
+                        <div className="status-details">{data.stats[2].base_stat}</div>
                       </div>
                       <div className="status">
                         <div>Sp Atk: </div>
-                        <div>{data.stats[3].base_stat}</div>
+                        <div className="status-details">{data.stats[3].base_stat}</div>
                       </div>
                       <div className="status">
                         <div>Sp Def: </div>
-                        <div>{data.stats[4].base_stat}</div>
+                        <div className="status-details">{data.stats[4].base_stat}</div>
                       </div>
                       <div className="status">
                         <div>Speed: </div>
-                        <div>{data.stats[5].base_stat}</div>
+                        <div className="status-details">{data.stats[5].base_stat}</div>
                       </div>
                    </div>
 
